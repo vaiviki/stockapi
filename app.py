15f300b6e3d7d9b8483a43b97ca822f29069d0fd
jivvi0 <virali232@gmail.com>
2024-03-08 23:23:06 +0530

    changes added

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # app.py
-import datetime
+from datetime import datetime
 
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
@@ -26,18 +26,19 @@ class StockModel(BaseModel):
 @app.post("/predict/")
 async def predict(data: StockModel):
     # Convert the string to a datetime object
-    date_object = datetime.strptime(StockModel.traded_date, "%Y-%m-%d")
+    date_object = datetime.strptime(data.traded_date, "%Y-%m-%d")
 
     # Get the weekday (Monday is 0 and Sunday is 6)
     weekday_number = date_object.weekday()
     pred_params = {
-        'Open': [StockModel.open],
-        'High': [StockModel.high],
-        'Low': [StockModel.low],
-        'Close': [StockModel.close],
-        'Shares Traded': [StockModel.shares_traded],
-        'Turnover': [StockModel.volume],
+        'Open': [data.open],
+        'High': [data.high],
+        'Low': [data.low],
+        'Close': [data.close],
+        'Shares Traded': [data.shares_traded],
+        'Turnover': [data.volume],
         'Weekday': [3]}
+    print(pred_params)
 
     return {"prediction": train.getPredictions(pred_params)}
 
--- tests/api_test.py
+++ tests/api_test.py
@@ -1,1 +1,18 @@
+import requests
+
+# Define input data
+input_data = {
+    'open': 22505.30,
+    'high': 22525.65,
+    'low': 22430.00,
+    'close': 22493.55,
+    'shares_traded': 379865142,
+    'volume': 33558.65,
+    'traded_date':'2024-03-07'}
+
+# Send POST request to FastAPI endpoint
+response = requests.post("http://127.0.0.1:8080/predict/", json=input_data)
+
+# Print prediction
+print("######",response.text)
 
